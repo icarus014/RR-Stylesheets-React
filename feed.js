ae69eca11e975871bf222bef822173cc6012b00b
Darian Brown <browndarian011@gmail.com>
2023-03-28 23:42:07 -0400

    commit

---
+++ feed.js
@@ -0,0 +1,15 @@
+function Feed () {
+    const s = {
+        'color': 'red'
+    }
+
+    return(
+        <div>
+            <p>Hi</p>
+        </div>
+    )
+}
+
+
+
+export default Feed
--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
+// import Button from 'react-bootstrap/Button'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Feed from 'feed'
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
+const styledText = {
+  fontSize:'25px',
+  color: 'white',
+
 }
+export default function App () {
+  return(
+    <div className='App'>
+    <Container>
+        <Navbar style={styledText} expand="lg" variant="light" bg="dark">
+          <Container>
+            <Navbar.Brand href="#">Home</Navbar.Brand>
+            <Navbar.Brand href="#">About Me</Navbar.Brand>
+            <Navbar.Brand href="#">Placement Pricing</Navbar.Brand>
+            <Navbar.Brand href="#">Socials</Navbar.Brand>
+          </Container>
+        </Navbar>
+    </Container>
+    </div>
+  ) 
 
-export default App;
+};
